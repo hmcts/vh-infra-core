9d6a3717374610436e4b7e421659caec6cf9785e
lowflying <chriscm.neu@gmail.com>
2021-11-26 16:49:44 +0000

    testing with a hardcoded subnet

--- terraform/09-video-hearing-core/modules/PrivateEndpoint/inputs.tf
+++ terraform/09-video-hearing-core/modules/PrivateEndpoint/inputs.tf
@@ -2,16 +2,17 @@ variable "environment" {
   type = string
 }
 
+variable "tags" {
+  type = map(any)
+  default = {}
+}
+
 variable "resource_group_name" {
   type = string
 }
 
 variable "location" {
   type = string
-}
-
-variable "vnet_name" {
-    type = string
 }
 
 variable "subnet_name" {
