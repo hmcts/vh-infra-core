5f7d4ce66f49f443edf529d556a0c7cc60d10912
lowflying <chriscm.neu@gmail.com>
2021-11-30 15:57:25 +0000

    paramterised the subnet biit for priv endpoints

--- terraform/09-video-hearing-core/modules/PrivateEndpoint/main.tf
+++ terraform/09-video-hearing-core/modules/PrivateEndpoint/main.tf
@@ -7,13 +7,19 @@ data "azurerm_key_vault" "vh-infra-core-kv" {
   resource_group_name = var.resource_group_name
 }
 
+data "azurerm_subnet" "ss_subnet" {
+  name                  = "vh_private_endpoints"
+  virtual_network_name  = "ss-${var.environment}-vnet"
+  resource_group_name   = "ss-${var.environment}-network-rg"
+}
+
 resource "azurerm_private_endpoint" "vh_endpoint" {
   for_each            = var.resources
 
   name                = format("vh-endpoint-%s-%s", lookup(each.value, "resource_name"), var.environment)
   location            = data.azurerm_resource_group.vh-infra-core.location
   resource_group_name = data.azurerm_resource_group.vh-infra-core.name
-  subnet_id           = var.subnet_id
+  subnet_id           = data.azurerm_subnet.ss_subnet.id
 
   private_service_connection {
     name                                = "vh-${var.environment}-aksserviceconnection"
