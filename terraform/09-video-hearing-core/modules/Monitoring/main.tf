b895489864bf4f6b4b29e9076d6e1bd4d811068d
lowflying <chriscm.neu@gmail.com>
2021-12-01 13:12:21 +0000

    reverted kv changes

--- terraform/09-video-hearing-core/modules/Monitoring/main.tf
+++ terraform/09-video-hearing-core/modules/Monitoring/main.tf
@@ -23,19 +23,19 @@ output "instrumentation_key" {
   value = azurerm_application_insights.vh-infra-core.instrumentation_key
 }
 
-#resource "azurerm_key_vault_secret" "applicationinsights" {
-#  name         = "applicationinsights--instrumentationkey"
-#  value        = azurerm_application_insights.vh-infra-core.instrumentation_key
-#  key_vault_id = data.azurerm_key_vault.vh-infra-core-kv.id
-#  tags = var.tags
-#}
-#
-#resource "azurerm_key_vault_secret" "appinsightskey" {
-#  name         = "azuread--appinsightskey"
-#  value        = azurerm_application_insights.vh-infra-core.instrumentation_key
-#  key_vault_id = data.azurerm_key_vault.vh-infra-core-kv.id
-#  tags = var.tags
-#}
+resource "azurerm_key_vault_secret" "applicationinsights" {
+  name         = "applicationinsights--instrumentationkey"
+  value        = azurerm_application_insights.vh-infra-core.instrumentation_key
+  key_vault_id = data.azurerm_key_vault.vh-infra-core-kv.id
+  tags = var.tags
+}
+
+resource "azurerm_key_vault_secret" "appinsightskey" {
+  name         = "azuread--appinsightskey"
+  value        = azurerm_application_insights.vh-infra-core.instrumentation_key
+  key_vault_id = data.azurerm_key_vault.vh-infra-core-kv.id
+  tags = var.tags
+}
 
 
 
