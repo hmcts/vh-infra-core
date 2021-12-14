fbf0e36630486ef9792196f1a505b324513aa09c
Thomas Watson <thomas.watson@version1.com>
2021-12-14 15:19:14 +0000

    Implement the no-purge rule

--- terraform/09-video-hearing-core/modules/KeyVaults/main.tf
+++ terraform/09-video-hearing-core/modules/KeyVaults/main.tf
@@ -9,6 +9,7 @@ locals {
 data "azurerm_client_config" "current" {}
 
 #### Per App Key Vault
+#tfsec:ignore:no-purge
 resource "azurerm_key_vault" "app_keyvaults" {
   for_each = var.keyvaults
 
@@ -18,7 +19,7 @@ resource "azurerm_key_vault" "app_keyvaults" {
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
-  purge_protection_enabled    = false #tfsec:ignore:no-purge
+  purge_protection_enabled    = false 
 
   sku_name = "standard"
   tags = var.tags
