5878a01247657f6fa6e3f78804782619a6a00d72
lowflying <chriscm.neu@gmail.com>
2022-01-14 12:16:25 +0000

    added kv mi

--- terraform/10-video-hearing-wowza/modules/wowza/vm_automation.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/vm_automation.tf
@@ -34,19 +34,25 @@ resource "azurerm_key_vault_secret" "configPassword" {
   key_vault_id = var.key_vault_id
 }
 
+data "azurerm_user_assigned_identity" "mi" {
+  name                  = "vh-vault-${var.environment}-mi"
+  resource_group_name   = "managed-identities-${var.environment}-rg"
+  location              = var.location
+}
 data "template_file" "cloudconfig" {
   template = file(var.cloud_init_file)
   vars = {
-    certPassword         = random_password.certPassword.result
-    certThumbprint       = upper(var.service_certificate_thumbprint)
-    storageAccountName   = azurerm_storage_account.wowza_recordings.name
-    storageContainerName = azurerm_storage_container.recordings.name
-    msiClientId          = var.storage_msi_client_id
-    restPassword         = md5("wowza:Wowza:${random_password.restPassword.result}")
-    streamPassword       = md5("wowza:Wowza:${random_password.streamPassword.result}")
-    keyVaultName         = "vh-infra-core-${var.environment}" 
-    certName             = "vh-${var.environment}-le-cert"
-    domain               = "hearings.reform.hmcts.net"
+    certPassword            = random_password.certPassword.result
+    certThumbprint          = upper(var.service_certificate_thumbprint)
+    storageAccountName      = azurerm_storage_account.wowza_recordings.name
+    storageContainerName    = azurerm_storage_container.recordings.name
+    msiClientId             = var.storage_msi_client_id
+    restPassword            = md5("wowza:Wowza:${random_password.restPassword.result}")
+    streamPassword          = md5("wowza:Wowza:${random_password.streamPassword.result}")
+    keyVaultName            = "vh-infra-core-${var.environment}" 
+    certName                = "vh-${var.environment}-le-cert"
+    domain                  = "hearings.reform.hmcts.net"
+    managedIdentityClientId = data.azurerm_user_assigned_identity.mi.client_id
   }
 }
 
