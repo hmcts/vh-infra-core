bf20b2b2aea662e5dc3297580fcf142a3385bdb8
lowflying <chriscm.neu@gmail.com>
2022-01-20 11:50:58 +0000

    fixed hearings-dns provider

--- terraform/10-video-hearing-wowza/00-init.tf
+++ terraform/10-video-hearing-wowza/00-init.tf
@@ -19,11 +19,11 @@ provider "azurerm" {
 provider "azurerm" {
   alias   = "dns"
   features {}
-  tenant_id     = var.dns_tenant_id
-  client_id     = var.dns_client_id
-  client_secret = var.dns_client_secret
-
-  subscription_id = var.dns_subscription_id
+  #tenant_id     = var.dns_tenant_id
+  #client_id     = var.dns_client_id
+  #client_secret = var.dns_client_secret
+#
+  #subscription_id = var.dns_subscription_id
 }
 
 provider "azurerm" {
