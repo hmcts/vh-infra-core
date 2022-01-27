9462f603cc461bfdf2e39db26db3601155755659
lowflying <chriscm.neu@gmail.com>
2022-01-27 15:11:43 +0000

    refactoring wowza endpoints

--- terraform/10-video-hearing-wowza/modules/wowza/private_endpoint.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/private_endpoint.tf
@@ -41,29 +41,19 @@ data "azurerm_storage_account" "wowza_sa" {
 
 ## Create private endpoint for Wowza VM's
 
-resource "azurerm_private_endpoint" "wowza_vm_endpoint_aks" {
-  name                = "vh-wowza-endpoint-${var.environment}"
-  location            = data.azurerm_resource_group.vh-infra-wowza.location
-  resource_group_name = data.azurerm_resource_group.vh-infra-wowza.name
-  subnet_id           = data.azurerm_subnet.ss_subnet.id
+#resource "azurerm_private_endpoint" "wowza_vm_endpoint_aks" {
+#  name                = "vh-wowza-endpoint-${var.environment}"
+#  location            = data.azurerm_resource_group.vh-infra-wowza.location
+#  resource_group_name = data.azurerm_resource_group.vh-infra-wowza.name
+#  subnet_id           = data.azurerm_subnet.ss_subnet.id
+#
+#  private_service_connection {
+#    name                           = "wowza-${var.environment}-aksserviceconnection"
+#    private_connection_resource_id = data.azurerm_private_link_service.wowza.id
+#    is_manual_connection           = false
+#  }
+#}
 
-  private_service_connection {
-    name                           = "wowza-${var.environment}-aksserviceconnection"
-    private_connection_resource_id = data.azurerm_private_link_service.wowza.id
-    is_manual_connection           = false
-  }
-}
-
-# BLANKED OUT AS PERMISSION NEEDS GRANTING TO RUN AGAINST SUBSCRTIPTION 4bb049c8-33f3-4860-91b4-9ee45375cc18 before the DNS chanes will work
-# Code blocks for this reside in 20-main.tf, 00-init.tf & private_endpoint.tf
-resource "azurerm_dns_a_record" "wowza_vm_endpoint_dns" {
-  provider = azurerm.private-endpoint-dns
-  name                = "vh-wowza-sds-${var.environment}"
-  zone_name           = var.private_dns_zone_group_name
-  resource_group_name = "core-infra-intsvc-rg" #"vh-hearings-reform-hmcts-net-dns-zone"
-  ttl                 = 300
-  records             = [azurerm_private_endpoint.wowza_vm_endpoint_aks.private_service_connection[0].private_ip_address]
-}
 
 ## Create Wowza Storage endpoint for AKS
 
@@ -85,4 +75,17 @@ resource "azurerm_private_endpoint" "wowza_storage_endpoint_aks" {
     private_dns_zone_ids = [var.private_dns_zone_group]
   }
 }
+
+
+# BLANKED OUT AS PERMISSION NEEDS GRANTING TO RUN AGAINST SUBSCRTIPTION 4bb049c8-33f3-4860-91b4-9ee45375cc18 before the DNS chanes will work
+# Code blocks for this reside in 20-main.tf, 00-init.tf & private_endpoint.tf
+resource "azurerm_dns_a_record" "wowza_storage_endpoint_dns" {
+  provider            = azurerm.private-endpoint-dns
+  name                = "vh-wowza-storage-${var.environment}"
+  zone_name           = var.private_dns_zone_group
+  resource_group_name = "core-infra-intsvc-rg" #"vh-hearings-reform-hmcts-net-dns-zone"
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.wowza_storage_endpoint_aks.private_service_connection[0].private_ip_address]
+}
+
 
