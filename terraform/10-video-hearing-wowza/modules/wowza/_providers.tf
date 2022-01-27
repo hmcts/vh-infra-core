c59f7e22e63bd789f6c7af7967645ad8e5cd15f0
lowflying <chriscm.neu@gmail.com>
2022-01-27 14:35:51 +0000

    testing provider set-up

--- terraform/10-video-hearing-wowza/modules/wowza/_providers.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/_providers.tf
@@ -20,9 +20,9 @@ provider "azurerm" {
   client_secret   = var.network_client_secret
   tenant_id       = var.network_tenant_id
 }
-provider "azurerm" {
-  features {}
-  alias           = "private-endpoint-dns"
-  skip_provider_registration = "true"
-  subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
-} 
+#provider "azurerm" {
+#  features {}
+#  alias           = "private-endpoint-dns"
+#  skip_provider_registration = "true"
+#  subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+#} 
