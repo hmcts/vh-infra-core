2c21d3927e2fb3f128fa2511b35657b0d7191ac2
lowflying <chriscm.neu@gmail.com>
2022-01-28 12:08:53 +0000

    added IP records from private endpoint

--- terraform/09-video-hearing-core/modules/PrivateEndpoint/main.tf
+++ terraform/09-video-hearing-core/modules/PrivateEndpoint/main.tf
@@ -43,7 +43,7 @@ variable "dns_zone_mapping" {
 resource "azurerm_private_dns_a_record" "endpoint-dns" {
   for_each            = var.resources
   name                = format("endpoint-%s", lookup(each.value, "resource_name"))
-  zone_name           = "${lookup(var.dns_zone_mapping, lookup(each.value, resource_type))}"
+  zone_name           = "${lookup(var.dns_zone_mapping, var.resource_type)}"
   resource_group_name = "core-infra-intsvc-rg"
   ttl                 = 300
   records             = azurerm_private_endpoint.vh_endpoint[each.key].private_service_connection[0].private_ip_address
