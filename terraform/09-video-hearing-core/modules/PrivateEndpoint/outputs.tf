8d964da9dfacec27bb4731be1b0d9d41636ba731
lowflying <chriscm.neu@gmail.com>
2022-01-31 15:00:27 +0000

    more var changes

--- terraform/09-video-hearing-core/modules/PrivateEndpoint/outputs.tf
+++ terraform/09-video-hearing-core/modules/PrivateEndpoint/outputs.tf
@@ -1,10 +1,10 @@
-output "endpoint_resource" {
-  value = tomap({
-    for k, e in azurerm_private_endpoint.vh_endpoint : k => {
-      resource_id   = e.id
-      name          = e.name
-      resource_type = k.private_service_connection.subresource_names
-      value         = k.private_service_connection
-    }
-  })
-}
+#output "endpoint_resource" {
+#  value = tomap({
+#    for k, e in azurerm_private_endpoint.vh_endpoint : k => {
+#      resource_id   = e.id
+#      resource_name = e.name
+#      resource_type = k.private_service_connection.subresource_names
+#      value         = k.private_service_connection
+#    }
+#  })
+#}
