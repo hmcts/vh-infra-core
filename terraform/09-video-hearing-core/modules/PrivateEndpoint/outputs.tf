de667c4d3cb5b8c14521477afd12debde40b06f3
lowflying <chriscm.neu@gmail.com>
2022-01-31 16:36:35 +0000

    more var changes

--- terraform/09-video-hearing-core/modules/PrivateEndpoint/outputs.tf
+++ terraform/09-video-hearing-core/modules/PrivateEndpoint/outputs.tf
@@ -4,7 +4,7 @@ output "endpoint_resource" {
       resource_id   = e.id
       resource_name = e.name
       resource_type = k.private_service_connection.subresource_names
-      value         = k.private_service_connection.private_ip_address.0.ip
+      value         = k.private_service_connection.private_ip_address.0
     }
   })
 }
