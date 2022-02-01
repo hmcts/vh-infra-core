91fde1a9e4161f2a4f4f6ba8e38849e47ba17164
lowflying <chriscm.neu@gmail.com>
2022-02-01 14:21:37 +0000

    dynamic zone mapping

--- terraform/09-video-hearing-core/modules/PrivateEndpoint/main.tf
+++ terraform/09-video-hearing-core/modules/PrivateEndpoint/main.tf
@@ -44,14 +44,15 @@ variable "dns_zone_mapping" {
 }
 
 
+
 resource azurerm_dns_a_record "test" {
   for_each = {for e, o in azurerm_private_endpoint.vh_endpoint : e => o.name }
   
   name                = each.key
-  zone_name           = lookup(var.dns_zone_mapping, "endpoint-Signal")
+  zone_name           = lookup(var.dns_zone_mapping, each.value.private_service_connection[0].subresource_names)
   resource_group_name = "core-infra-intsvc-rg"
   ttl                 = 3600
-  records             = [each.value.private_service_connection[0].private_ip_address]
+  records             = [each.value.private_service_connection[1].private_ip_address]
 }
 
 
