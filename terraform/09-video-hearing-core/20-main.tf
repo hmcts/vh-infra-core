23aa9f420463f6f620f6f62f8dc9602cc0eb05d2
lowflying <chriscm.neu@gmail.com>
2022-02-01 10:59:25 +0000

    testing a new wayy to create the A rec

--- terraform/09-video-hearing-core/20-main.tf
+++ terraform/09-video-hearing-core/20-main.tf
@@ -267,13 +267,13 @@ variable "dns_zone_mapping" {
 #}
 
 resource azurerm_dns_a_record "test" {
-  for_each = module.vh_endpoint
-
-  name                = vh_endpoint[each.value].name
+  for_each = {for e, o in module.vh_endpoint : e => o.name }
+  
+  name                = each.key
   zone_name           = "privatelink.database.windows.net"
   resource_group_name = "core-infra-intsvc-rg"
   ttl                 = 3600
-  records             = [vh_endpoint[each.value].private_service_connection[0].private_ip_address]
+  records             = [each.value.private_service_connection[0].private_ip_address]
 }
 
 module vh_kv_endpoint {
