a689c3c1bda5b84283a4082dd622333d43f0475f
lowflying <chriscm.neu@gmail.com>
2022-02-01 11:06:53 +0000

    testing a new wayy to create the A rec

--- terraform/09-video-hearing-core/modules/PrivateEndpoint/outputs.tf
+++
@@ -1,9 +0,0 @@
-#output "endpoint_resource" {
-#  value = tomap({
-#    for k, e in azurerm_private_endpoint.vh_endpoint : k => {
-#      resource_id   = e.id
-#      resource_name = e.name
-#      resource_type = e.private_service_connection.0
-#    }
-#  })
-#}
