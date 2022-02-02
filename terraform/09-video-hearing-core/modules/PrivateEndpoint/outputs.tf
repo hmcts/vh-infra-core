ed45c481c895a7259e0ca4b96b4a6de2c9f7876d
lowflying <chriscm.neu@gmail.com>
2022-02-02 09:00:44 +0000

    cleaning up (again)

--- terraform/09-video-hearing-core/modules/PrivateEndpoint/outputs.tf
+++ terraform/09-video-hearing-core/modules/PrivateEndpoint/outputs.tf
@@ -3,7 +3,7 @@ output "endpoint_resource" {
     for k, v in azurerm_private_endpoint.vh_endpoint : k => {
       resource_id   = v.id
       resource_name = v.name
-      resource_type = v.private_service_connection[0].subresource_names
+      resource_type = v.private_service_connection[0].subresource_names[0]
       resource_ip   = v.private_service_connection[0].private_ip_address
     }
   })
