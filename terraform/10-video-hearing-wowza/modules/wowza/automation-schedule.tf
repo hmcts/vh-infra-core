d4c7c7c44c413a3b3367ced23f66df8762efd0d2
Thomas Watson <thomas.watson@version1.com>
2022-02-07 11:23:36 +0000

    Tag all the things

--- terraform/10-video-hearing-wowza/modules/wowza/automation-schedule.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/automation-schedule.tf
@@ -24,9 +24,13 @@ resource "azurerm_automation_runbook" "wowza-VM-runbook" {
   description             = "This is a runbook used to stop and start wowza VMs"
   runbook_type            = "PowerShellWorkflow"
   content                 = "./VM-Automation-Files/wowza-vm-runbook.ps1"
+  
   publish_content_link {
     uri = ""
   }
+
+  tags                     = var.tags
+  
   depends_on = [
     azurerm_resource_group_template_deployment.wowza-automation-acct
   ]
@@ -43,6 +47,8 @@ resource "azurerm_automation_schedule" "wowza-automation-schedule" {
 
   start_time              = each.value.time
   description             = "This is a schedule to ${each.value.action} wowza VMs at ${each.value.time}"
+
+  tags                    = var.tags
   depends_on = [
     azurerm_resource_group_template_deployment.wowza-automation-acct
   ]
@@ -54,6 +60,7 @@ resource "azurerm_automation_job_schedule" "runbook-schedule-job" {
   automation_account_name = "wowza-automation-acct-${var.environment}"
   schedule_name           = "wowza-${each.value.action}-schedule-${var.environment}"
   runbook_name            = "wowza-VM-runbook-${var.environment}"
+   tags                   = var.tags
 
 # vmlist = azurerm_linux_virtual_machine.wowza[*].name
   parameters = {
