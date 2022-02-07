43b0fccd367899c56aacb96ef56a71258076fcab
Thomas Watson <thomas.watson@version1.com>
2022-02-07 11:50:03 +0000

    Updat eterraform for_each to reference action key

--- terraform/10-video-hearing-wowza/modules/wowza/automation-schedule.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/automation-schedule.tf
@@ -56,7 +56,7 @@ resource "azurerm_automation_schedule" "wowza-automation-schedule" {
 resource "azurerm_automation_job_schedule" "runbook-schedule-job" {
   for_each                = local.schedule_action
   resource_group_name     = azurerm_resource_group.wowza.name
-  automation_account_name = "wowza-automation-acct-${var.environment}"
+  automation_account_name = azurerm_automation_schedule.wowza-automation-schedule[each.action]
   schedule_name           = "wowza-${each.value.action}-schedule-${var.environment}"
   runbook_name            = azurerm_automation_schedule.wowza-automation-schedule.name
 
