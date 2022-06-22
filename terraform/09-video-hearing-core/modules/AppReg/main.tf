9207217d783c11fc836309aa15d1ca6326b7baa3
Christopher Pateman <christopher.pateman@version1.com>
2022-06-22 15:33:29 +0100

    add identifier uri

--- terraform/09-video-hearing-core/modules/AppReg/main.tf
+++ terraform/09-video-hearing-core/modules/AppReg/main.tf
@@ -91,7 +91,7 @@ resource "azurerm_key_vault_secret" "secret" {
 resource "azurerm_key_vault_secret" "secret" {
   for_each = var.app_conf
   name     = "azuread--identifieruri"
-  value    =  each.value.identifier_uris[0]
+  value    = each.value.identifier_uris[0]
   #key_vault_id = data.azurerm_key_vault.key_vault[each.key].id
   key_vault_id = var.app_keyvaults_map[each.key].id
   # FromTFSec
