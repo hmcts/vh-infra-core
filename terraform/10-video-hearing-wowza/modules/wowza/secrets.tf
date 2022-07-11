02dd455b9f21b6c3119dac34353cc961886c79c6
Christopher Pateman <christopher.pateman@version1.com>
2022-07-11 09:53:52 +0100

    fix formatting

--- terraform/10-video-hearing-wowza/modules/wowza/secrets.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/secrets.tf
@@ -1,16 +1,16 @@
 locals {
   secret_prefix = "wowzaconfiguration"
   secrets = {
-    "managedidentityclientid" = azurerm_user_assigned_identity.wowza_storage.client_id,
-    "storageaccountkey" = azurerm_storage_account.wowza_recordings.primary_access_key,
-    "restPassword" = random_password.restPassword.result,
-    "streamPassword" = random_password.streamPassword.result,
-    "azure-storage-directory" = "/wowzadata/azurecopy",
-    "endpoint" = "https://${local.wowza_domain}:443",
-    "storage-account" = azurerm_storage_account.wowza_recordings.name,
-    "storage-account-endpoint" = azurerm_storage_account.wowza_recordings.primary_blob_endpoint,
+    "managedidentityclientid"   = azurerm_user_assigned_identity.wowza_storage.client_id,
+    "storageaccountkey"         = azurerm_storage_account.wowza_recordings.primary_access_key,
+    "restPassword"              = random_password.restPassword.result,
+    "streamPassword"            = random_password.streamPassword.result,
+    "azure-storage-directory"   = "/wowzadata/azurecopy",
+    "endpoint"                  = "https://${local.wowza_domain}:443",
+    "storage-account"           = azurerm_storage_account.wowza_recordings.name,
+    "storage-account-endpoint"  = azurerm_storage_account.wowza_recordings.primary_blob_endpoint,
     "storage-account-container" = azurerm_storage_container.recordings.name,
-    "username" = var.admin_user
+    "username"                  = var.admin_user
 
   }
 }
