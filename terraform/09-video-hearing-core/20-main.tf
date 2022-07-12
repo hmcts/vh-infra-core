0c80bddaecee0bfb6c28e8cd072719581c5f781f
Your Name <james.ferrari@version1.com>
2022-07-12 08:53:33 +0100

    amend db name

--- terraform/09-video-hearing-core/20-main.tf
+++ terraform/09-video-hearing-core/20-main.tf
@@ -295,7 +295,7 @@ module "VHDataServices" {
       performance_level = "S0"
     }
 
-    vhtestapi = {
+    vhtest = {
       collation         = "SQL_Latin1_General_CP1_CI_AS"
       edition           = "Standard"
       performance_level = "S0"
