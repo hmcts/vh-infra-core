1387cbf1f7c3778e11ce21ad1bfb1e9d372d41ef
Christopher Pateman <christopher.pateman@version1.com>
2022-07-15 14:10:31 +0100

    Add SSH rule

--- terraform/10-video-hearing-wowza/modules/wowza/lb-public.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/lb-public.tf
@@ -84,7 +84,7 @@ resource "azurerm_lb_rule" "ssh" {
   backend_port                   = 22
   frontend_ip_configuration_name = local.frontend_ip_configuration_name
   probe_id                       = azurerm_lb_probe.wowza_rtmps-public.id
-  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.wowza_vm[0].id]
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.wowza-public.id]
   load_distribution              = "Default"
   idle_timeout_in_minutes        = 30
 }
