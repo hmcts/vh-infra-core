beedd97e50479ec536a0fffa0b1eca4f90faae33
Christopher Pateman <christopher.pateman@version1.com>
2022-07-15 09:59:12 +0100

    Add network watcher

---
+++ terraform/10-video-hearing-wowza/modules/wowza/network_logs.tf
@@ -0,0 +1,33 @@
+data "azurerm_log_analytics_workspace" "core" {
+  name                = var.service_name
+  resource_group_name = var.service_name
+}
+
+resource "azurerm_network_watcher" "wowza" {
+  name                = "${var.service_name}-watcher"
+  location            = azurerm_resource_group.wowza.location
+  resource_group_name = azurerm_resource_group.wowza.name
+}
+
+resource "azurerm_network_watcher_flow_log" "nsg" {
+  network_watcher_name = azurerm_network_watcher.wowza.name
+  resource_group_name  = azurerm_resource_group.wowza.name
+  name                 = "${var.service_name}-flow-logs"
+
+  network_security_group_id = azurerm_network_security_group.wowza.id
+  storage_account_id        = azurerm_storage_account.wowza_recordings.id
+  enabled                   = true
+
+  retention_policy {
+    enabled = true
+    days    = 7
+  }
+
+  traffic_analytics {
+    enabled               = true
+    workspace_id          = azurerm_log_analytics_workspace.core.workspace_id
+    workspace_region      = azurerm_log_analytics_workspace.core.location
+    workspace_resource_id = azurerm_log_analytics_workspace.core.id
+    interval_in_minutes   = 10
+  }
+}
