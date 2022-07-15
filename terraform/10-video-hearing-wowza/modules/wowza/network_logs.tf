ec22bb9943bb11205c822fdcf5676b503433995a
Christopher Pateman <christopher.pateman@version1.com>
2022-07-15 10:26:37 +0100

    Correct naming for watcher

--- terraform/10-video-hearing-wowza/modules/wowza/network_logs.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/network_logs.tf
@@ -4,9 +4,10 @@ data "azurerm_log_analytics_workspace" "core" {
 }
 
 resource "azurerm_network_watcher_flow_log" "nsg" {
-  network_watcher_name = "NetworkWatcher_${azurerm_resource_group.wowza.location}" ## Subscription Shared Watcher
-  resource_group_name  = azurerm_resource_group.wowza.name
-  name                 = "${var.service_name}-flow-logs"
+  network_watcher_name = "NetworkWatcher_${azurerm_resource_group.wowza.location}"
+  resource_group_name  = "NetworkWatcherRG"
+
+  name = "${var.service_name}-flow-logs"
 
   network_security_group_id = azurerm_network_security_group.wowza.id
   storage_account_id        = azurerm_storage_account.wowza_recordings.id
