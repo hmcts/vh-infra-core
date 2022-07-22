5762be825d659d48fd9b64545a3e447c6bcf52c7
Christopher Pateman <christopher.pateman@version1.com>
2022-07-22 08:12:03 +0100

    fmt

--- terraform/10-video-hearing-wowza/modules/wowza/network.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/network.tf
@@ -78,7 +78,7 @@ resource "azurerm_network_security_group" "wowza" {
     protocol                     = "Tcp"
     source_port_range            = "*"
     destination_port_range       = "443,8087"
-    source_address_prefix        = lookup(locals.aks_address, var.environment, "*")
+    source_address_prefix        = lookup(local.aks_address, var.environment, "*")
     destination_address_prefixes = local.ip_list
   }
 
