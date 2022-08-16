50c763abc373124ebaac865752b2d35b03f79aed
Anish Govind <anish.govind@version1.com>
2022-08-16 15:06:44 +0100

    add

--- terraform/10-video-hearing-wowza/modules/wowza/_data_sources.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/_data_sources.tf
@@ -1,36 +1,36 @@
-# Splunk
-variable "install_splunk_uf" {
-  default = false
-}
+# # Splunk
+# variable "install_splunk_uf" {
+#   default = false
+# }
 
-data "azurerm_key_vault" "soc_vault" {
-  count    = var.install_splunk_uf ? 1 : 0
-  provider = azurerm.soc
+# data "azurerm_key_vault" "soc_vault" {
+#   count    = var.install_splunk_uf ? 1 : 0
+#   provider = azurerm.soc
 
-  name                = "soc-prod"
-  resource_group_name = "soc-core-infra-prod-rg"
-}
+#   name                = "soc-prod"
+#   resource_group_name = "soc-core-infra-prod-rg"
+# }
 
-data "azurerm_key_vault_secret" "splunk_username" {
-  count    = var.install_splunk_uf ? 1 : 0
-  provider = azurerm.soc
+# data "azurerm_key_vault_secret" "splunk_username" {
+#   count    = var.install_splunk_uf ? 1 : 0
+#   provider = azurerm.soc
 
-  name         = "splunk-gui-admin-username"
-  key_vault_id = data.azurerm_key_vault.soc_vault[0].id
-}
+#   name         = "splunk-gui-admin-username"
+#   key_vault_id = data.azurerm_key_vault.soc_vault[0].id
+# }
 
-data "azurerm_key_vault_secret" "splunk_password" {
-  count    = var.install_splunk_uf ? 1 : 0
-  provider = azurerm.soc
+# data "azurerm_key_vault_secret" "splunk_password" {
+#   count    = var.install_splunk_uf ? 1 : 0
+#   provider = azurerm.soc
 
-  name         = "splunk-gui-admin-password"
-  key_vault_id = data.azurerm_key_vault.soc_vault[0].id
-}
+#   name         = "splunk-gui-admin-password"
+#   key_vault_id = data.azurerm_key_vault.soc_vault[0].id
+# }
 
-data "azurerm_key_vault_secret" "splunk_pass4symmkey" {
-  count    = var.install_splunk_uf ? 1 : 0
-  provider = azurerm.soc
+# data "azurerm_key_vault_secret" "splunk_pass4symmkey" {
+#   count    = var.install_splunk_uf ? 1 : 0
+#   provider = azurerm.soc
 
-  name         = "Splunk-pass4SymmKey"
-  key_vault_id = data.azurerm_key_vault.soc_vault[0].id
-}
+#   name         = "Splunk-pass4SymmKey"
+#   key_vault_id = data.azurerm_key_vault.soc_vault[0].id
+# }
