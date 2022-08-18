cba6f2aea848f2f24b23b6e45fa2b111aa4c5e0b
Anish Govind <anish.govind@version1.com>
2022-08-18 16:15:21 +0100

    add

--- terraform/10-video-hearing-wowza/modules/wowza/vm_splunk_extension.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/vm_splunk_extension.tf
@@ -28,6 +28,6 @@ module "splunk-uf" {
   virtual_machine_id         = azurerm_linux_virtual_machine.wowza[count.index].id
   splunk_username            = local.splunk_admin_username
   splunk_password            = random_password.splunk_admin_password.result
-  tags                       = local.common_tags
+  tags                       = var.tags
   # splunk_pass4symmkey        = try(data.azurerm_key_vault_secret.splunk_pass4symmkey[0].value, null)
 }
