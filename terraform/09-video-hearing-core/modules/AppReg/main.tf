d8906ce9a927da771b8f3b5a39ef16ecfbbccd8a
Christopher Pateman <christopher.pateman@version1.com>
2022-08-18 11:27:37 +0100

    Update id token

--- terraform/09-video-hearing-core/modules/AppReg/main.tf
+++ terraform/09-video-hearing-core/modules/AppReg/main.tf
@@ -24,14 +24,14 @@ resource "azuread_application" "app_reg" {
   identifier_uris = [for item in each.value.identifier_uris :
   var.environment == "prod" ? replace(item, ".prod.", ".") : replace(item, "stg", "staging")]
 
-  implicit_grant {
-    id_token_issuance_enabled = true
-  }
-  
+
   web {
     homepage_url = var.environment == "prod" ? replace("https://${each.key}.${var.environment}.platform.hmcts.net", ".prod.", ".") : replace("https://${each.key}.${var.environment}.platform.hmcts.net", "stg", "staging")
     redirect_uris = [for item in each.value.reply_urls_web :
     var.environment == "prod" ? replace(item, ".prod.", ".") : replace(item, "stg", "staging")]
+    implicit_grant {
+      id_token_issuance_enabled = true
+    }
   }
   single_page_application {
     redirect_uris = [for item in each.value.reply_urls_spa :
