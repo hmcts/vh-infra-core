3ea482b95463aba50f1dcb5521c4a63b27cfbf4b
Anish Govind <anish.govind@version1.com>
2022-08-18 11:30:17 +0100

    add

--- terraform/10-video-hearing-wowza/modules/wowza/vm_splunk_extension.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/vm_splunk_extension.tf
@@ -9,12 +9,23 @@ resource "random_password" "splunk_admin_password" {
 }
 
 
+# module "splunk-uf" {
+#   for_each                   = tomap(azurerm_linux_virtual_machine.wowza)
+#   source                     = "git::https://github.com/hmcts/terraform-module-splunk-universal-forwarder.git?ref=VIH-8976"
+#   auto_upgrade_minor_version = true
+#   virtual_machine_type       = "vm"
+#   virtual_machine_id         = each.value.id
+#   splunk_username            = local.splunk_admin_username
+#   splunk_password            = random_password.splunk_admin_password.result
+#   # splunk_pass4symmkey        = try(data.azurerm_key_vault_secret.splunk_pass4symmkey[0].value, null)
+# }
+
 module "splunk-uf" {
-  for_each                   = tomap(azurerm_linux_virtual_machine.wowza)
+  count                      = length(azurerm_linux_virtual_machine.wowza)
   source                     = "git::https://github.com/hmcts/terraform-module-splunk-universal-forwarder.git?ref=VIH-8976"
   auto_upgrade_minor_version = true
   virtual_machine_type       = "vm"
-  virtual_machine_id         = each.value.id
+  virtual_machine_id         = azurerm_linux_virtual_machine.wowza[count.index].id
   splunk_username            = local.splunk_admin_username
   splunk_password            = random_password.splunk_admin_password.result
   # splunk_pass4symmkey        = try(data.azurerm_key_vault_secret.splunk_pass4symmkey[0].value, null)
