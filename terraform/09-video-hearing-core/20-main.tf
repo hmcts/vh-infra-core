e94919edd15a65675b180296b2bca76867a9ea46
Christopher Pateman <christopher.pateman@version1.com>
2022-08-19 13:38:34 +0100

    Remove output

--- terraform/09-video-hearing-core/20-main.tf
+++ terraform/09-video-hearing-core/20-main.tf
@@ -306,9 +306,7 @@ module "AppReg" {
   ]
   tags = local.common_tags
 }
-output "app_sp" {
-  value = module.AppReg.app_sp
-}
+
 #--------------------------------------------------------------
 # VH - Monitoring
 #--------------------------------------------------------------
