2b4b105cd8b36116298f16e297710508a55ca9cf
Thomas Watson <thomas.watson@version1.com>
2022-09-15 11:52:47 +0100

    Added DTS SDS Devs group policy to vh-infra-core

--- terraform/09-video-hearing-core/modules/KeyVaults/main.tf
+++ terraform/09-video-hearing-core/modules/KeyVaults/main.tf
@@ -355,6 +355,80 @@ resource "azurerm_key_vault" "vh-infra-core-ht" {
 
 }
 
+resource "azurerm_key_vault_access_policy" "dts_sds_dev_infrakv" {
+  count = var.environment != "prod" ? 1 : 0
+
+  key_vault_id = azurerm_key_vault.vh-infra-core-ht.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = "7bde62e7-b39f-487c-95c9-b4c794fdbb96" # DTS SDS Developers AD Group
+
+  certificate_permissions = [
+    "Backup",
+    "Create",
+    "Delete",
+    "DeleteIssuers",
+    "Get",
+    "GetIssuers",
+    "Import",
+    "List",
+    "ListIssuers",
+    "ManageContacts",
+    "ManageIssuers",
+    "Purge",
+    "Recover",
+    "Restore",
+    "SetIssuers",
+    "Update"
+  ]
+
+  key_permissions = [
+    "Backup",
+    "Create",
+    "Decrypt",
+    "Delete",
+    "Encrypt",
+    "Get",
+    "Import",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Sign",
+    "UnwrapKey",
+    "Update",
+    "Verify",
+    "WrapKey"
+  ]
+
+  secret_permissions = [
+    "Backup",
+    "Delete",
+    "Get",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Set"
+  ]
+
+  storage_permissions = [
+    "Backup",
+    "Delete",
+    "DeleteSAS",
+    "Get",
+    "GetSAS",
+    "List",
+    "ListSAS",
+    "Purge",
+    "Recover",
+    "RegenerateKey",
+    "Restore",
+    "Set",
+    "SetSAS",
+    "Update"
+  ]
+}
+
 # kv user identity
 resource "azurerm_key_vault_access_policy" "kv_user_identity" {
 
