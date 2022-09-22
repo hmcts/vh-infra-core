8b3a5023ef481eaf84476ebb19260f4e42cf52ec
Thomas Watson <thomas.watson@version1.com>
2022-09-22 15:08:24 +0100

    Changed to AzPI provider

--- terraform/09-video-hearing-core/modules/SignalR/main.tf
+++ terraform/09-video-hearing-core/modules/SignalR/main.tf
@@ -1,55 +1,124 @@
-# resource "azurerm_signalr_service" "vh" {
-#   name                = var.resource_prefix
-#   resource_group_name = var.resource_group_name
-#   location            = var.location
+resource "azapi_resource" "symbolicname" {
+  type = "Microsoft.SignalRService/signalR@2022-02-01"
+  name = "string"
+  location = "string"
+  parent_id = "string"
+  tags = {
+    tagName1 = "tagValue1"
+    tagName2 = "tagValue2"
+  }
+  identity {
+    type = "string"
+    identity_ids = []
+  }
+  body = jsonencode({
+    properties = {
+      cors = {
+        allowedOrigins = [
+          "string"
+        ]
+      }
+      disableAadAuth = bool
+      disableLocalAuth = bool
+      features = [
+        {
+          flag = "string"
+          properties = {}
+          value = "string"
+        }
+      ]
+      liveTraceConfiguration = {
+        categories = [
+          {
+            enabled = "string"
+            name = "string"
+          }
+        ]
+        enabled = "string"
+      }
+      networkACLs = {
+        defaultAction = "string"
+        privateEndpoints = [
+          {
+            allow = [
+              "string"
+            ]
+            deny = [
+              "string"
+            ]
+            name = "string"
+          }
+        ]
+        publicNetwork = {
+          allow = [
+            "string"
+          ]
+          deny = [
+            "string"
+          ]
+        }
+      }
+      publicNetworkAccess = "string"
+      resourceLogConfiguration = {
+        categories = [
+          {
+            enabled = "string"
+            name = "string"
+          }
+        ]
+      }
+      tls = {
+        clientCertEnabled = bool
+      }
+      upstream = {
+        templates = [
+          {
+            auth = {
+              managedIdentity = {
+                resource = "string"
+              }
+              type = "string"
+            }
+            categoryPattern = "string"
+            eventPattern = "string"
+            hubPattern = "string"
+            urlTemplate = "string"
+          }
+        ]
+      }
+    }
+    sku = {
+      capacity = int
+      name = "string"
+      tier = "string"
+    }
+    kind = "string"
+  })
+}
 
-#   sku {
-#     name     = "Premium_P1"
-#     capacity = 1
-#   }
-#   tags = var.tags
-# }
-
-resource "azurerm_resource_group_template_deployment" "ARMdeploy-signalR-MI" {
-  name                = var.resource_prefix
-  resource_group_name = var.resource_group_name
-
-  # "Incremental" ADDS the resource to already existing resources. "Complete" destroys all other resources and creates the new one
-  deployment_mode = "Incremental"
-
-  # the parameters below can be found near the top of the ARM file
-  parameters_content = jsonencode({
-    "tw_test_signalr_name" = {
-      value = local.signalr_name
-    },
-    "my_location" = {
-      value = var.location
-    },
-    "tags" = {
-      value = var.tags
-    },
-    "sku_name" = {
-      value = locals.sku_name
-    },
-    "sku_type" = {
-      value = locals.sku_type
-    },
-    "sku_size" = {
-      value = locals.sku_size
-    },
-    "userAssigned_identity" = {
-      value = azurerm_user_assigned_identity.signalr-managed-id.id # need this info
-    },
-    "signalr_cd_name" = {
-      value = var.resource_prefix
-    },
-    "signalr_custom_certificate_id" = {
-      value = locals.custom_cert_id
-    },
-    "signalr_custom_domain" = {
-      value = locals.signalr_custom_domain
+resource "azapi_resource" "symbolicname" {
+  type = "Microsoft.SignalRService/signalR/customDomains@2022-02-01"
+  name = "string"
+  parent_id = "string"
+  body = jsonencode({
+    properties = {
+      customCertificate = {
+        id = "string"
+      }
+      domainName = "string"
     }
   })
-  # the actual ARM template file we will use
-  template_content = file("signalr-template.json")
+}
+
+resource "azapi_resource" "symbolicname" {
+  type = "Microsoft.SignalRService/signalR/customCertificates@2022-02-01"
+  name = "string"
+  parent_id = "string"
+  body = jsonencode({
+    properties = {
+      keyVaultBaseUri = "string"
+      keyVaultSecretName = "string"
+      keyVaultSecretVersion = "string"
+    }
+  })
 }
