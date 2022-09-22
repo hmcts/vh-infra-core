b2f91882a2468de18efd4c43c8b3ced48a9a01bc
Thomas Watson <thomas.watson@version1.com>
2022-09-22 12:47:29 +0100

    Added template and variables

--- terraform/09-video-hearing-core/modules/SignalR/main.tf
+++ terraform/09-video-hearing-core/modules/SignalR/main.tf
@@ -1,12 +1,55 @@
-resource "azurerm_signalr_service" "vh" {
+# resource "azurerm_signalr_service" "vh" {
+#   name                = var.resource_prefix
+#   resource_group_name = var.resource_group_name
+#   location            = var.location
+
+#   sku {
+#     name     = "Premium_P1"
+#     capacity = 1
+#   }
+#   tags = var.tags
+# }
+
+resource "azurerm_resource_group_template_deployment" "ARMdeploy-signalR-MI" {
   name                = var.resource_prefix
   resource_group_name = var.resource_group_name
-  location            = var.location
 
-  sku {
-    name     = local.sku.name
-    capacity = local.sku.capacity
-  }
-  tags = var.tags
+  # "Incremental" ADDS the resource to already existing resources. "Complete" destroys all other resources and creates the new one
+  deployment_mode = "Incremental"
+
+  # the parameters below can be found near the top of the ARM file
+  parameters_content = jsonencode({
+    "tw_test_signalr_name" = {
+      value = local.signalr_name
+    },
+    "my_location" = {
+      value = var.location
+    },
+    "tags" = {
+      value = var.tags
+    },
+    "sku_name" = {
+      value = locals.sku_name
+    },
+    "sku_type" = {
+      value = locals.sku_type
+    },
+    "sku_size" = {
+      value = locals.sku_size
+    },
+    "userAssigned_identity" = {
+      value = azurerm_user_assigned_identity.signalr-managed-id.id # need this info
+    },
+    "signalr_cd_name" = {
+      value = var.resource_prefix
+    },
+    "signalr_custom_certificate_id" = {
+      value = locals.custom_cert_id
+    },
+    "signalr_custom_domain" = {
+      value = locals.signalr_custom_domain
+    }
+  })
+  # the actual ARM template file we will use
+  template_content = file("signalr-template.json")
 }
-
--- terraform/09-video-hearing-core/modules/SignalR/variables.tf
+++ terraform/09-video-hearing-core/modules/SignalR/variables.tf
@@ -1,51 +1,63 @@
-variable "environment" {}
+# variable "environment" {}
 
-variable "environment_to_sku_map" {
-  type = map(any)
-  default = {
-    AAT = {
-      name     = "Standard_S1"
-      capacity = 1
-    }
-    Demo = {
-      name     = "Standard_S1"
-      capacity = 1
-    }
-    Dev = {
-      name     = "Free_F1"
-      capacity = 1
-    }
-    Preview = {
-      name     = "Free_F1"
-      capacity = 1
-    }
-    Sandbox = {
-      name     = "Standard_S1"
-      capacity = 1
-    }
-    Test1 = {
-      name     = "Free_F1"
-      capacity = 1
-    }
-    Test2 = {
-      name     = "Free_F1"
-      capacity = 1
-    }
-    PreProd = {
-      name     = "Standard_S1"
-      capacity = 1
-    }
-    Prod = {
-      name     = "Standard_S1"
-      capacity = 1
-    }
-  }
-}
+# variable "environment_to_sku_map" {
+#   type = map(any)
+#   default = {
+#     AAT = {
+#       name     = "Standard_S1"
+#       capacity = 1
+#     }
+#     Demo = {
+#       name     = "Standard_S1"
+#       capacity = 1
+#     }
+#     Dev = {
+#       name     = "Free_F1"
+#       capacity = 1
+#     }
+#     Preview = {
+#       name     = "Free_F1"
+#       capacity = 1
+#     }
+#     Sandbox = {
+#       name     = "Standard_S1"
+#       capacity = 1
+#     }
+#     Test1 = {
+#       name     = "Free_F1"
+#       capacity = 1
+#     }
+#     Test2 = {
+#       name     = "Free_F1"
+#       capacity = 1
+#     }
+#     PreProd = {
+#       name     = "Standard_S1"
+#       capacity = 1
+#     }
+#     Prod = {
+#       name     = "Standard_S1"
+#       capacity = 1
+#     }
+#   }
+# }
 
 locals {
   environment = var.environment
-  sku = lookup(var.environment_to_sku_map, var.environment, {
-    name     = "Standard_S1"
-    capacity = 1
-  })
+  # sku = lookup(var.environment_to_sku_map, var.environment, {
+  #   name     = "Standard_S1"
+  #   capacity = 1
+  # })
+
+  sku_name = "Premium_P1"
+
+  sku_type = "Premium"
+
+  sku_size = "P1"
+
+  signalr_cd_name = "" # needed
+
+  custom_cert_id = "" # needed
+
+  signalr_custom_domain = "" # needs a conditional
 }
