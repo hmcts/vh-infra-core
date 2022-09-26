7ecb535dc18584fe4a4b4450c6995bcc896c5186
Your Name <james.ferrari@version1.com>
2022-09-26 15:24:14 +0100

    revert changes to public lb

--- terraform/10-video-hearing-wowza/modules/wowza/lb-public.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/lb-public.tf
@@ -1,7 +1,6 @@
 locals {
   frontend_ip_configuration_name = "wowza"
 }
-
 resource "azurerm_public_ip" "wowza" {
   name                = var.service_name
   resource_group_name = azurerm_resource_group.wowza.name
@@ -9,7 +8,6 @@ resource "azurerm_public_ip" "wowza" {
   allocation_method   = "Static"
   sku                 = "Standard"
   tags                = var.tags
-  domain_name_label   = var.service_name
 }
 
 resource "azurerm_lb" "wowza-public" {
@@ -26,9 +24,23 @@ resource "azurerm_lb" "wowza-public" {
   }
   tags = var.tags
 }
-resource "azurerm_lb_backend_address_pool" "wowza-public" {
+
+resource "azurerm_lb_probe" "wowza_rtmps-public" {
   loadbalancer_id = azurerm_lb.wowza-public.id
-  name            = "Wowza-Virtual-Machines"
+  name            = "rtmps-running-probe"
+  port            = 443
+}
+
+resource "azurerm_lb_probe" "wowza_rest-public" {
+  loadbalancer_id = azurerm_lb.wowza-public.id
+  name            = "rest-running-probe"
+  port            = 8087
+}
+
+resource "azurerm_lb_probe" "wowza_ssh-public" {
+  loadbalancer_id = azurerm_lb.wowza-public.id
+  name            = "ssh-probe"
+  port            = 22
 }
 
 resource "azurerm_lb_rule" "wowza-public" {
@@ -44,6 +56,32 @@ resource "azurerm_lb_rule" "wowza-public" {
   idle_timeout_in_minutes        = 30
 }
 
+resource "azurerm_lb_rule" "wowza_rest-public" {
+  count = var.wowza_instance_count
+
+  loadbalancer_id                = azurerm_lb.wowza-public.id
+  name                           = "REST-${count.index}"
+  protocol                       = "Tcp"
+  frontend_port                  = 8090 + count.index
+  backend_port                   = 8087
+  frontend_ip_configuration_name = local.frontend_ip_configuration_name
+  probe_id                       = azurerm_lb_probe.wowza_rest-public.id
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.wowza_vm-public[count.index].id]
+}
+
+resource "azurerm_lb_backend_address_pool" "wowza-public" {
+  loadbalancer_id = azurerm_lb.wowza-public.id
+  name            = "wowza"
+}
+
+resource "azurerm_lb_backend_address_pool" "wowza_vm-public" {
+  count = var.wowza_instance_count
+
+  loadbalancer_id = azurerm_lb.wowza-public.id
+  name            = "${var.service_name}-${count.index}"
+}
+
+
 resource "azurerm_lb_rule" "ssh" {
   loadbalancer_id                = azurerm_lb.wowza-public.id
   name                           = "SSH-Rule"
@@ -55,16 +93,4 @@ resource "azurerm_lb_rule" "ssh" {
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.wowza-public.id]
   load_distribution              = "Default"
   idle_timeout_in_minutes        = 30
-}
-
-resource "azurerm_lb_probe" "wowza_rtmps-public" {
-  loadbalancer_id = azurerm_lb.wowza-public.id
-  name            = "RTMPS-Probe"
-  port            = 443
-}
-
-resource "azurerm_lb_probe" "wowza_ssh-public" {
-  loadbalancer_id = azurerm_lb.wowza-public.id
-  name            = "SSH-Probe"
-  port            = 22
 }
