b3d0a392c0f7eebb3c3705afbaf5707e3cef9259
Thomas Watson <thomas.watson@version1.com>
2022-11-28 14:21:04 +0000

    Changed environment var and added data references

--- terraform/10-video-hearing-wowza/modules/wowza/wowza_lb_alerts.tf
+++ terraform/10-video-hearing-wowza/modules/wowza/wowza_lb_alerts.tf
@@ -1,30 +1,30 @@
 data "azurerm_monitor_action_group" "dev" {
-  resource_group_name = "vh-infra-core-${env}"
-  name                = "Vh-Action-Group-dev-${var.env}"
+  resource_group_name = "vh-infra-core-${var.environment}"
+  name                = "Vh-Action-Group-dev-${var.environment}"
 }
 
 data "azurerm_monitor_action_group" "devops" {
-  resource_group_name = "vh-infra-core-${env}"
-  name                = "Vh-Action-Group-devops-${var.env}"
+  resource_group_name = "vh-infra-core-${var.environment}"
+  name                = "Vh-Action-Group-devops-${var.environment}"
 }
 
 locals {
   wowzaLoadBalancers = {
     private = {
       scope = [azurerm_lb.wowza.id]
-      name  = "vh-wowza-lb-private-alert-"
+      name  = "vh-wowza-lb-private-alert-${var.environment}"
     }
     public = {
       scope = [azurerm_lb.wowza-public.id]
-      name  = "vh-wowza-lb-public-alert-"
+      name  = "vh-wowza-lb-public-alert-${var.environment}"
     }
   }
 }
 
 resource "azurerm_monitor_metric_alert" "wowza_lb_alert" {
-  for_each = var.env == "prod" ? local.wowzaLoadBalancers : {}
+  for_each = var.environment == "prod" ? local.wowzaLoadBalancers : {}
 
-  name                = "${each.value.name}${var.env}"
+  name                = "${each.value.name}"
   resource_group_name = var.resource_group_name
   scopes              = each.value.scope
   description         = "Wowza Load Balancer Health is Below 95%, Please Investigate ASAP as this may impact the service."
@@ -42,10 +42,10 @@ resource "azurerm_monitor_metric_alert" "wowza_lb_alert" {
   window_size = "PT5M"
 
   action {
-    action_group_id = azurerm_monitor_action_group.dev.id
+    action_group_id = data.azurerm_monitor_action_group.dev.id
   }
 
   action {
-    action_group_id = azurerm_monitor_action_group.devops.id
+    action_group_id = data.azurerm_monitor_action_group.devops.id
   }
 }
