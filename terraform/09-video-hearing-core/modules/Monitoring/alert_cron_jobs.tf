60a5b15698da74b0baf1fcdf57fdf5aae4dfd5c0
Your Name <james.ferrari@version1.com>
2023-01-09 15:27:25 +0000

    amend cron job alerts

--- terraform/09-video-hearing-core/modules/Monitoring/alert_cron_jobs.tf
+++ terraform/09-video-hearing-core/modules/Monitoring/alert_cron_jobs.tf
@@ -40,7 +40,7 @@ locals {
 }
 
 resource "azurerm_monitor_scheduled_query_rules_alert_v2" "cron_jobs" {
-  for_each = local.cron_jobs
+  for_each = var.env == "prod" ? local.cron_jobs : {}
 
   name         = "VH - SDS - CronJob ${each.key} Failure ${title(var.env)}"
   display_name = "VH - SDS - CronJob ${each.key} Failure ${title(var.env)}"
