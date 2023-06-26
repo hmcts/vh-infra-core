1132117764b381dafe89262a0ffe6ab4d4789487
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-06-26 16:15:47 +0100

    adding ai connection string

--- terraform/modules/Monitoring/outputs.tf
+++ terraform/modules/Monitoring/outputs.tf
@@ -1,3 +1,7 @@
 output "instrumentation_key" {
   value = azurerm_application_insights.vh-infra-core.instrumentation_key
 }
+
+output "ai_connectionstring" {
+  value = azurerm_application_insights.vh-infra-core.connection_string
+}
