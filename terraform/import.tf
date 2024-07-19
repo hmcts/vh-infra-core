847f13570dd10912cb7e0835361471ab24f8c8e3
SwapnaVersion1 <157039140+SwapnaVersion1@users.noreply.github.com>
2024-07-19 16:54:34 +0100

    import formatted

--- terraform/import.tf
+++ terraform/import.tf
@@ -1,4 +1,4 @@
 import {
-  to = module.VHDataServices.azurerm_role_assignment.Azure_Service_Bus_Data_Receiver_premium[0] 
+  to = module.VHDataServices.azurerm_role_assignment.Azure_Service_Bus_Data_Receiver_premium[0]
   id = "/subscriptions/5ca62022-6aa2-4cee-aaa7-e7536c8d566c/resourceGroups/vh-infra-core-prod/providers/Microsoft.ServiceBus/namespaces/vh-infra-core-prod-premium/providers/Microsoft.Authorization/roleAssignments/e8f55300-9fae-4d52-940c-66693edef7d1"
 }
