c27a9907c28447d4e33396c5d44e0fae9b3f20c6
SwapnaVersion1 <157039140+SwapnaVersion1@users.noreply.github.com>
2024-07-22 09:42:30 +0100

    import the sbus and role assignment

---
+++ terraform/import.tf
@@ -0,0 +1,10 @@
+import {
+  to = module.VHDataServices.azurerm_role_assignment.Azure_Service_Bus_Data_Receiver_premium[0]
+  id = "/subscriptions/5ca62022-6aa2-4cee-aaa7-e7536c8d566c/resourceGroups/vh-infra-core-prod/providers/Microsoft.ServiceBus/namespaces/vh-infra-core-prod-premium/providers/Microsoft.Authorization/roleAssignments/e8f55300-9fae-4d52-940c-66693edef7d1"
+}
+
+import {
+  to = module.VHDataServices.azurerm_servicebus_namespace.vh-infra-core-premium[0]
+  id = "/subscriptions/5ca62022-6aa2-4cee-aaa7-e7536c8d566c/resourceGroups/vh-infra-core-prod/providers/Microsoft.ServiceBus/namespaces/vh-infra-core-prod-premium"
+}
+
