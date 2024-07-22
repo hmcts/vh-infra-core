6daa9cbfb9812d11fb94281e5f79399f197e7811
SwapnaVersion1 <157039140+SwapnaVersion1@users.noreply.github.com>
2024-07-22 13:49:54 +0100

    added import

--- terraform/modules/VHDataServices/main.tf
+++ terraform/modules/VHDataServices/main.tf
@@ -144,22 +144,24 @@ resource "azurerm_role_assignment" "Azure_Service_Bus_Data_Receiver" {
 
 resource "azurerm_servicebus_namespace" "vh-infra-core-premium" {
   count               = local.environment == "prod" ? 1 : 0
+  capacity  =1 
+  zone_redundant                      = true
   name                = "vh-infra-core-prod-premium"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Premium"
   tags                = var.tags
 }
-/*
+
 resource "azurerm_servicebus_queue" "vh-infra-core-premium" {
   for_each     = var.queues
   name         = each.key
-  namespace_id = azurerm_servicebus_namespace.vh-infra-core-premium[0].id
+  namespace_id = "/subscriptions/5ca62022-6aa2-4cee-aaa7-e7536c8d566c/resourceGroups/vh-infra-core-prod/providers/Microsoft.ServiceBus/namespaces/vh-infra-core-prod-premium"
   #namespace_name      = azurerm_servicebus_namespace.vh-infra-core.name
   enable_partitioning   = false
   lock_duration         = "PT5M"
   max_size_in_megabytes = 1024
-} */
+} 
 
 resource "azurerm_role_assignment" "Azure_Service_Bus_Data_Receiver_premium" {
   count                = local.environment == "prod" ? 1 : 0
