fbf30ce3d4e03b913bca529d5b3a75e63162fc2a
Sam Kompfner <sam.kompfner@hmcts.net>
2025-06-11 15:55:20 +0100

    add prod contributor role assignment for group

--- terraform/20-main.tf
+++ terraform/20-main.tf
@@ -8,6 +8,17 @@ resource "azurerm_resource_group" "vh-infra-core" {
   tags     = local.common_tags
 }
 
+###############################################################
+# Wowza RG Group Access #######################################
+###############################################################
+
+resource "azurerm_role_assignment" "dts_vh_contributors_prod_access" {
+  count                = var.environment == "prod" ? 1 : 0
+  scope                = azurerm_resource_group.vh-infra-core.id
+  role_definition_name = "Contributor"
+  principal_id         = data.azuread_group.dts_vh_contributors_prod.object_id
+}
+
 #--------------------------------------------------------------
 # VH - KeyVaults
 #--------------------------------------------------------------
